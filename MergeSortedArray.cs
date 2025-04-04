9708239dec3a86e3797947f686dc1e6c62a76fe3
Gaura Palande <Gaurav.Palande147@gmail.com>
2025-04-04 10:56:55 -0700

    Merge Sorted Array

---
+++ MergeSortedArray.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections.Generic;
+
+public class Solution{
+  
+   public static int[] MergeSorted(int[] nums1, int m, int[] nums2, int n) 
+   {
+        int p1 = m - 1;
+        int p2 = n - 1;
+
+        for (int p = m + n - 1; p >= 0; p--)
+        {
+            if (p2 < 0)
+            {
+                break;
+            }
+            if (p1 >= 0 && nums1[p1] > nums2[p2])
+            {
+                nums1[p] = nums1[p1];
+                p1 -= 1;
+            }
+            else
+            {
+                nums1[p] = nums2[p2];
+                p2 -= 1;
+            }
+        }
+        return nums1;
+    }
+}
